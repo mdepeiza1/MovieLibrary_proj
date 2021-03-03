40703e26033f03f4b17e784638359a5d0ee790d7
Dustin <adustin70@gmail.com>
2021-03-03 12:13:50 -0500

    Added functionality to MovieController Get action/methods and tested with postman

---
+++ WebAPISample/Migrations/20210303165103_movies.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPISample.Migrations
+{
+    public partial class movies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movies",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Genre", "Title" },
+                values: new object[] { 1, "Martin Scorsese", "Drama", "The Departed" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Genre", "Title" },
+                values: new object[] { 2, "Christoper Nolan", "Drama", "The Dark Knight" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Genre", "Title" },
+                values: new object[] { 3, "Christopher Nolan", "Drama", "Inception" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movies");
+        }
+    }
+}
