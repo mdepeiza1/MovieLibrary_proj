3e0cfc45c9d06a0dfebc3aa45735256114a7b16c
Key Admin <mdepeiza@keyconceptskb.com>
2021-03-03 14:31:03 -0500

    Movie list is diaplayed on home page

--- Client/app.js
+++ Client/app.js
@@ -1,11 +1,12 @@
 (function($){
     function processForm( e ){
         var dict = {
-        	Title : this["title"].value,
-        	Director: this["director"].value
+            Title : this["title"].value,
+            Genre : this["genre"].value,
+            Director: this["director"].value
         };
 
-        $.ajax({
+        /*$.ajax({
             url: 'https://localhost:44325/api/movie',
             dataType: 'json',
             type: 'post',
@@ -17,10 +18,25 @@
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
-        });
+        });*/
+
+      
 
         e.preventDefault();
     }
 
+    var $movies = $('#movie-list');
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        type: 'GET',
+        success: function(movies){
+            $.each(movies, function(i, movie){
+                $movies.append('<tr><td>' + movie.title + '</td><td>' + movie.genre + '</td><td>' + movie.director + '</td></tr>');
+            });
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
     $('#my-form').submit( processForm );
 })(jQuery);
