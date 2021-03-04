95d82f539c2f7720264387aac3803f79ef5a78d9
Dustin <adustin70@gmail.com>
2021-03-04 10:55:51 -0500

    Added some button styling to the index can move or change if it causes issues later

--- Client/app.js
+++ Client/app.js
@@ -6,22 +6,6 @@
             Director: this["director"].value
         };
 
-        /*$.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });*/
-
-      
-
         e.preventDefault();
     }
 
@@ -38,5 +22,62 @@
             console.log( errorThrown );
         }
     });
+    $('#my-form').submit( processForm );
+})(jQuery);
+
+(function($){
+    function addButton( a ){
+        var dict = {
+            Title : this["title"].value,
+            Genre : this["genre"].value,
+            Director : this["director"].value
+        };
+
+        a.preventDefault();
+    }
+        var $movies = $('#movie-list');
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function(movie){
+                $movies.append('<tr><td>' + movie.title + '</td><td>' + movie.genre + '</td><td>' + movie.director + '</td></tr>')
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+        $('#my-form').submit( addButton );
+    
+})(jQuery);
+
+
+(function($){
+    function processForm( u ){
+        var dict = {
+        	Title : this["title"].value,
+            Genre : this["genre"].value,
+        	Director: this["director"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        u.preventDefault();
+    }
+
     $('#my-form').submit( processForm );
 })(jQuery);
