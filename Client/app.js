04f0abdaca1055eaa3694617f2e8084dc39c1773
Dustin <adustin70@gmail.com>
2021-03-05 10:20:55 -0500

    Added delete function to app.js and background image to index.html

--- Client/app.js
+++ Client/app.js
@@ -1,15 +1,4 @@
 (function($){
-    /*function processForm( e ){
-        var dict = {
-            MovieID : this["movieId"].value,
-            Title : this["title"].value,
-            Genre : this["genre"].value,
-            Director: this["director"].value
-        };
-
-        e.preventDefault();
-    }*/
-
     var $movies = $('#movie-list');
     $.ajax({
         url: 'https://localhost:44325/api/movie',
@@ -23,19 +12,6 @@
             console.log( errorThrown );
         }
     });
-
-
-
-
-
-
-
-
-
-
-
-
-    //$('#my-form').submit( processForm );
 })(jQuery);
 
 
@@ -92,7 +68,7 @@ $('#update').on('click',function(){
 
 })(jQuery);
 
-(function($){//implement this
+(function($){
      $('#details').on('click',function(e){
          var dict = {
              movieId : parseInt($('#movieId').val())
@@ -104,10 +80,6 @@ $('#update').on('click',function(){
             dataType: 'json',
             contentType: 'application/json',
             success: function(movie){
-               //$('#titleDetail').value = movie.title;
-               //$('#genreDetail').value = movie.genre;
-               //$('#directorDetail').value = movie.director;
-
                 alert('Title: ' + movie.title + ' Genre: ' + movie.genre + ' Director: ' + movie.director);
             },
             error: function( jqXhr, textStatus, errorThrown ){
@@ -118,61 +90,25 @@ $('#update').on('click',function(){
      });
  })(jQuery);
 
-
-//$('#update').on('click',function(){
-    /*var movie = {
-        Title : $('#title'),
-        Genre : $('#genre'),
-        Director: $('#director')
-    };*/
-
-    /*
-    var dict = {
-       movieId : $('#my-form').movieId.value,
-        Title :  $('#my-form').title.value,
-        Genre :  $('#my-form').genre.value,
-        Director: $('#my-form').director.value
-    };*/
-/*
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        type: 'PUT',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            title : $('#title').val(),
-        genre : $('#genre').val(),
-        director: $('#director').val()
-    }),
-        success: function(data, textStatus, jQxhr ){
-            $('#response pre').html( data );
-            console.log(data);
-        },
-        error: function( jqXhr, textStatus, errorThrown ){
-            alert('error');
-            console.log( errorThrown );
+ (function($){
+    $('#delete').on('click',function(){
+        let dict = {
+            movieId : parseInt($('#movieId').val())
         }
-    });
-
-});*/
-
-/*
-$('#delete').on('click',function(){
-
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        type: 'DELETE',
-        dataType: 'json',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            movieId : $('#movieId').val(),
-    }),
-        success: function(data, textStatus, jQxhr ){
-            $('#response pre').html( data );
-            console.log(data);
-        },
-        error: function( jqXhr, textStatus, errorThrown ){
-            alert('error');
+        $.ajax({
+            url: 'https://localhost:44325/api/movie/'+ parseInt($('#movieId').val()),
+            type: 'DELETE',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function(data, textStatus, jQxhr ){
+                $movies.remove( data );
+                console.log(data);
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                alert('error');
+                console.log( errorThrown );
+            }
         });
     });
-});*/
+    })(jQuery);
